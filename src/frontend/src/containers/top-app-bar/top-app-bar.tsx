f6d0e25ce8a5113d51d68c884706f0f273c31896
Lasse Nitschke Duer <LNK@kmd.dk>
2021-02-04 13:21:56 +0100

    Add layout and top app bar

---
+++ src/frontend/src/containers/top-app-bar/top-app-bar.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
+
+export default function TopAppBar() {
+  const [open, setOpen] = React.useState(false);
+  const classes = useStyles();
+
+  const openLoginDialog = () => {
+    setOpen(true);
+  };
+
+  const closeLoginDialog = () => {
+    setOpen(false);
+  }
+
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Be 1337!
+          </Typography>
+          <Button color="inherit" onClick={openLoginDialog}>Login</Button>
+        </Toolbar>
+      </AppBar>
+
+
+      <Dialog open={open} onClose={closeLoginDialog} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Login</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To become 1337 you must login!
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="email"
+            label="Email"
+            type="email"
+            fullWidth
+          />
+          <TextField
+            margin="dense"
+            id="password"
+            label="Password"
+            type="password"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closeLoginDialog} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={closeLoginDialog} color="primary">
+            Login
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
