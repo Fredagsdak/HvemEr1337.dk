e3f795ffda8996f47eb732c1d31cd801bcaf3986
Lasse Nitschke Duer <LNK@kmd.dk>
2021-02-16 22:15:26 +0100

    Add scoreboard

--- src/frontend/src/containers/layout/layout.tsx
+++ src/frontend/src/containers/layout/layout.tsx
@@ -5,6 +5,7 @@ import { PageNotFound404 } from './../page-not-found-404/page-not-found-404';
 import { Route, Switch } from "react-router-dom";
 import { Routes } from './../../shared/routes';
 import Post1337 from "../post1337/post1337";
+import ScoreBoard from '../score-board/score-board';
 
 function Layout(): JSX.Element {
     return (
@@ -13,6 +14,7 @@ function Layout(): JSX.Element {
             <Switch>
                 <Route exact path={Routes.root} component={Main} />
                 <Route exact path={Routes.be1337} component={Post1337} />
+                <Route exact path={Routes.scoreBoard} component={ScoreBoard} />
                 <Route path="*" component={PageNotFound404} />
             </Switch>
         </>
---
+++ src/frontend/src/containers/score-board/score-board.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ScoreBoard: React.FC = () => {
+    return <div>
+        score board
+    </div>;
+}
+
+export default ScoreBoard;
