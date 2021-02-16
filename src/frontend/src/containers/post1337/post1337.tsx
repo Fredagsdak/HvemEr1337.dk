30ba510317f2e6a7fce50af09796a446ecb5bb79
Lasse Nitschke Duer <LNK@kmd.dk>
2021-02-16 22:28:21 +0100

    route to scoreboard after post submit

--- src/frontend/src/containers/post1337/post1337.tsx
+++ src/frontend/src/containers/post1337/post1337.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { Dispatch } from 'redux';
-import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { addPost } from '../../store/action-creators';
+import { useHistory } from 'react-router-dom';
+import { Routes } from '../../shared/routes';
 
 type Props = {
     savePost: (post: IPost | any) => void;
@@ -17,9 +19,11 @@ const AddPost: React.FC<Props> = ({ savePost }) => {
             [e.currentTarget.id]: e.currentTarget.value
         });
     }
+    const history = useHistory();
     const addNewPost = (e: React.FormEvent) => {
         e.preventDefault();
         savePost(post);
+        history.push(Routes.scoreBoard);
     }
     return <form onSubmit={addNewPost}>
         <TextField id="post-message" label="Message" onChange={handlePostData} />
