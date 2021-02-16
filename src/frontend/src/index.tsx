1694259fbb93801f47c8a1b57da2156d102db06d
Lasse Nitschke Duer <LNK@kmd.dk>
2021-02-16 21:32:21 +0100

    Apply redux to application

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Layout from './containers/layout/layout';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware, Store } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import reducer from "./store/reducer";
+
+const store: Store<PostState, PostAction> & {
+  dispatch: DispatchType
+} = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Layout />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/frontend/src/store/reducer.ts
@@ -0,0 +1,25 @@
+import * as actionTypes from './action-types';
+
+const initialState: PostState = {
+    posts: []
+}
+
+const reducer = (
+    state: PostState = initialState,
+    action: PostAction
+): PostState => {
+    switch (action.type) {
+        case actionTypes.ADD_POST: {
+            const newPost: IPost = {
+                message: action.post.message
+            };
+            return {
+                ...state,
+                posts: state.posts.concat(newPost)
+            }
+        }
+        default: return state;
+    }
+}
+
+export default reducer;
